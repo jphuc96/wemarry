082efbb54ff4e5fdc5076f28a7fe04f1ff1fbf9d
Phuc Nguyen <phucnh@ghn.vn>
2023-01-22 23:38:52 +0700

    fix bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,17 @@
-import PhotoAlbum from "react-photo-album"
-import NextJsImage from "./NextJsImage"
+import PhotoAlbum, { RenderPhotoProps } from "react-photo-album"
+import Image from "next/image"
 
-// ./8.jpg JPEG 3024x4032 3024x4032+0+0 8-bit sRGB 4.5966MiB 0.000u 0:00.001
-// ./9.jpg JPEG 3024x4032 3024x4032+0+0 8-bit sRGB 4.01937MiB 0.000u 0:00.001
-// ./14.jpg JPEG 3024x4032 3024x4032+0+0 8-bit sRGB 1.57454MiB 0.000u 0:00.001
-// ./28.jpg JPEG 6000x4000 6000x4000+0+0 8-bit sRGB 4.00436MiB 0.000u 0:00.001
-// ./29.jpg JPEG 2305x1537 2305x1537+0+0 8-bit sRGB 341119B 0.000u 0:00.001
-// ./15.jpg JPEG 3024x4032 3024x4032+0+0 8-bit sRGB 1285660B 0.000u 0:00.000
-// ./17.jpg JPEG 3024x4032 3024x4032+0+0 8-bit sRGB 1.96241MiB 0.000u 0:00.001
-// ./16.jpg JPEG 3024x4032 3024x4032+0+0 8-bit sRGB 2.53875MiB 0.000u 0:00.000
-// ./12.jpg JPEG 3024x4032 3024x4032+0+0 8-bit sRGB 2.89989MiB 0.000u 0:00.001
-// ./13.jpg JPEG 3024x4032 3024x4032+0+0 8-bit sRGB 2.3663MiB 0.000u 0:00.001
-// ./11.jpg JPEG 3024x4032 3024x4032+0+0 8-bit sRGB 2.37773MiB 0.000u 0:00.001
-// ./10.jpg JPEG 3024x4032 3024x4032+0+0 8-bit sRGB 4623180B 0.000u 0:00.001
-// ./21.jpg JPEG 3024x4032 3024x4032+0+0 8-bit sRGB 3.14584MiB 0.000u 0:00.001
-// ./20.jpg JPEG 1536x2049 1536x2049+0+0 8-bit sRGB 840362B 0.000u 0:00.001
-// ./22.jpg JPEG 3024x4032 3024x4032+0+0 8-bit sRGB 2.29213MiB 0.000u 0:00.001
-// ./23.jpg JPEG 3024x4032 3024x4032+0+0 8-bit sRGB 1.654MiB 0.000u 0:00.001
-// ./27.jpg JPEG 4000x6000 4000x6000+0+0 8-bit sRGB 2.19806MiB 0.000u 0:00.001
-// ./26.jpg JPEG 6000x4000 6000x4000+0+0 8-bit sRGB 1.96002MiB 0.000u 0:00.001
-// ./32.jpg JPEG 2305x1537 2305x1537+0+0 8-bit sRGB 342623B 0.000u 0:00.001
-// ./18.jpg JPEG 2678x4015 2678x4015+0+0 8-bit sRGB 981122B 0.000u 0:00.001
-// ./24.jpg JPEG 4000x6000 4000x6000+0+0 8-bit sRGB 3.86427MiB 0.000u 0:00.001
-// ./30.jpg JPEG 4000x6000 4000x6000+0+0 8-bit sRGB 1.77784MiB 0.000u 0:00.001
-// ./31.jpg JPEG 4000x6000 4000x6000+0+0 8-bit sRGB 2.55442MiB 0.000u 0:00.001
-// ./25.jpg JPEG 4000x6000 4000x6000+0+0 8-bit sRGB 1.53558MiB 0.000u 0:00.001
-// ./19.jpg JPEG 3024x4032 3024x4032+0+0 8-bit sRGB 3.04184MiB 0.000u 0:00.001
-// ./4.jpg JPEG 1536x2049 1536x2049+0+0 8-bit sRGB 1.60329MiB 0.000u 0:00.001
-// ./5.jpg JPEG 1536x2049 1536x2049+0+0 8-bit sRGB 790256B 0.000u 0:00.001
-// ./7.jpg JPEG 3024x4032 3024x4032+0+0 8-bit sRGB 4.22667MiB 0.000u 0:00.001
-// ./6.jpg JPEG 3024x4032 3024x4032+0+0 8-bit sRGB 2.11449MiB 0.000u 0:00.001
-// ./2.jpg JPEG 1440x1800 1440x1800+0+0 8-bit sRGB 164754B 0.000u 0:00.000
-// ./3.jpg JPEG 1440x1800 1440x1800+0+0 8-bit sRGB 459115B 0.000u 0:00.000
-// ./1.jpg JPEG 1440x1800 1440x1800+0+0 8-bit sRGB 134873B 0.000u 0:00.000
+const NextJsImage: React.FC<RenderPhotoProps> = ({
+  imageProps: { src, alt, title, sizes, className, onClick },
+  wrapperStyle,
+}) => (
+  <div style={wrapperStyle}>
+    <div style={{ display: "block", position: "relative", width: "100%", height: "100%" }}>
+      <Image fill src={src} alt={alt} title={title} sizes={sizes} className={className} onClick={onClick} />
+    </div>
+  </div>
+)
 
-// sort above by number
 const photos = [
   { src: "/images/phuquoc/1.jpg", width: 1440, height: 1800 },
   { src: "/images/phuquoc/2.jpg", width: 1440, height: 1800 },
